07893d6043c4e98a1b653dd2f74769d5f8ee16f8
James Muscat <jamesremuscat@gmail.com>
2022-08-12 11:57:58 +0100

    Oops, don't override `_port`.

--- src/avista/serial.py
+++ src/avista/serial.py
@@ -12,15 +12,15 @@ class SerialProtocol(Protocol):
 class SerialDevice(Device):
     def __init__(self, config):
         super().__init__(config)
-        self._port = config.extra['port']
+        self._portstr = config.extra['port']
         self._baudrate = config.extra.get('baudrate', 9600)
         self._connect()
 
     def _connect(self):
-        self.log.info(f'Connecting to serial port {self._port}...')
+        self.log.info(f'Connecting to serial port {self._portstr}...')
         self._port = SerialPort(
             self.get_protocol(),
-            self._port,
+            self._portstr,
             reactor,
             baudrate=self._baudrate
         )
